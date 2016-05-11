ed857c58a2b939d3b1f4ac1bdf278380314fde46
qmm <qmmhao@sina.com>
2016-05-11 20:38:49 +0800

    html-dom-bootatrap-jquery

--- paper.js
+++ paper.js
@@ -1,7 +1,10 @@
 function check() {
-    var myClass = document.getElementById("myClass").value;
-    var mySno = document.getElementById("mySno").value;
-    var myName = document.getElementById("myName").value;
+    var myClass = $("#myClass").val();
+    var mySno = $("#mySno").val();
+    var myName = $("#myName").val();
+    var answer = ['统一建模语言','封装性','继承性','多态性'];
+    
+    
     if (!myClass) {
         alert("请输入班级!");
     }
@@ -13,56 +16,62 @@ function check() {
     }
     else {
         var sum = 0;
-        if (document.getElementById("answer1").value === "统一建模语言")
+        if ( gapFilling(answer,$("#answer1").val()))
             sum += 5;
-        if (document.getElementById("answer2").value === "封装性")
+        if (gapFilling(answer,$("#answer2").val()))
             sum += 5;
-        if (document.getElementById("answer3").value === "继承性")
+        if (gapFilling(answer,$("#answer3").val()))
             sum += 5;
-        if (document.getElementById("answer4").value === "多态性")
+        if (gapFilling(answer,$("#answer4").val()))
             sum += 5;
-        var element_1_1 = document.getElementById("element-1-1");
-        var element_1_2 = document.getElementById("element-1-2");
-        var element_2_1_1 = document.getElementById("element-2-1-1");
-        var element_2_1_2 = document.getElementById("element-2-1-2");
-        var element_2_1_3 = document.getElementById("element-2-1-3");
-        var element_2_2_1 = document.getElementById("element-2-2-1");
-        var element_2_2_2 = document.getElementById("element-2-2-2");
-        var element_2_2_3 = document.getElementById("element-2-2-3");
-        var element_3_1 = document.getElementById("element-3-1");
-        var element_3_2 = document.getElementById("element-3-2");
-        if (element_1_1.checked) {
-            sum += 5;
-        }
-        if (element_1_2.checked) {
+        var element_1_1 = $("#element-1-1");
+        var element_1_2 = $("#element-1-2");
+        var element_2_1_1 = $("#element-2-1-1");
+        var element_2_1_2 = $("#element-2-1-2");
+        var element_2_1_3 = $("#element-2-1-3");
+        var element_2_2_1 = $("#element-2-2-1");
+        var element_2_2_2 = $("#element-2-2-2");
+        var element_2_2_3 = $("#element-2-2-3");
+        var element_3_1 = $("#element-3-1");
+        var element_3_2 = $("#element-3-2");
+        if (element_1_1.prop("checked")) {
             sum += 5;
         }
-        if (element_2_1_1.checked) {
+        if (element_1_2.prop("checked")) {
             sum += 5;
         }
-        if (element_2_1_2.checked) {
+        if (element_2_1_1.prop("checked")) {
             sum += 5;
         }
-        if (element_2_1_3.checked) {
+        if (element_2_1_2.prop("checked")) {
             sum += 5;
         }
-        if (element_2_2_1.checked) {
+        if (element_2_1_3.prop("checked")) {
             sum += 5;
         }
-        if (element_2_2_2.checked) {
+        if (element_2_2_1.prop("checked")) {
             sum += 5;
         }
-        if (element_2_2_3.checked) {
+        if (element_2_2_2.prop("checked")) {
             sum += 5;
         }
-        if (element_3_1.checked) {
+        if (element_2_2_3.prop("checked")) {
             sum += 5;
         }
-        if (element_3_2.checked) {
+        if (element_3_1.prop("checked")) {
             sum += 5;
         }
-        document.getElementById("result").value = sum;
+        if (element_3_2.prop("checked")) {
+            sum += 5;
+        }
+        $("#result").val(sum);
     }
 }
 
- 
+ function gapFilling(answer,myAnswer){
+      for(var i = 0 ; i < answer.length ; i++){
+         if(myAnswer === answer[i]){
+             return true;
+         }
+     }
+ }
